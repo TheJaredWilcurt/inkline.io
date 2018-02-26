51f02d332e25ea1eecce84d5e16b951582312856
Alex Grozav <alex@grozav.com>
2018-02-26 22:10:30 +0200

    Added global stylus config.

--- helpers/utils.js
+++ helpers/utils.js
@@ -34,11 +34,10 @@ exports.vueStyleLoaders = function (options) {
         }
     };
 
-    console.log(path.resolve('src/css/index.styl'))
     const stylusOptions = {
         paths: [path.resolve('node_modules'), path.resolve('src')],
         import: [
-            path.resolve(__dirname, 'src', 'css', 'index.styl')
+            path.resolve('src', 'resources', 'css', 'index.styl')
         ],
         preferPathResolver: 'webpack',
         'include css': true,
