abbed609085453d1b56613864fbbea7765fdfc17
Alex <alex@grozav.com>
2018-11-22 16:16:24 +0200

    Added initial navbar documentation, unfinished.

---
+++ src/views/docs/components/NavbarView/script.js
@@ -0,0 +1,12 @@
+import View from '@components/View';
+import { IButton, INavbar, INavbarBrand } from 'inkline';
+
+export default {
+    name: 'NavbarView',
+    extends: View,
+    components: {
+        IButton,
+        INavbar,
+        INavbarBrand
+    }
+};
