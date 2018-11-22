954b156dcf1a85cf7953f10d20c42d1b0b89419a
Alex <alex@grozav.com>
2018-11-22 16:16:42 +0200

    Added documentation for navs.

---
+++ src/views/docs/components/NavView/script.js
@@ -0,0 +1,12 @@
+import View from '@components/View';
+import { IButton, INav, INavItem } from 'inkline';
+
+export default {
+    name: 'NavView',
+    extends: View,
+    components: {
+        IButton,
+        INav,
+        INavItem
+    }
+};
